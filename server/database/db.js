6b5c5dfb37f785d8848226b33201bafafe93610a
Fayaaz316 <111269768+Fayaaz316@users.noreply.github.com>
2023-06-03 18:58:09 +0600

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const Connection = () => {
 
-    const MONGODB_URI = `mongodb+srv://mern:todo123@mern-todo.vbs99ep.mongodb.net/?retryWrites=true&w=majority`;
+    const MONGODB_URI = `mongodb+srv://fayaazhassan316:F12345678@cluster0.igfj887.mongodb.net/?retryWrites=true&w=majority`;
 
     mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
